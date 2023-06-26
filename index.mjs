51a627dd6d18cdb482236b332b37e1aa02f9acdf
Josh Muir <hi@nezto.re>
2023-06-26 21:21:40 +0100

    Add body parsing

--- index.mjs
+++ index.mjs
@@ -1,11 +1,18 @@
 import express from 'express'
 import { inspect } from 'util'
+import {json} from 'body-parser'
 
 const app = express()
 const port = process.env.port || 3000
 
+app.use(json({
+  inflate: true,
+  limit: "10mb",
+  type: ["application/tlsrpt+gzip", ["application/tlsrpt+json"]]
+}))
+
 app.post('/v1/tls-rpt', (req, res) => {
-  console.log(inspect(req, true, null))
+  console.log(inspect(req.body, true, null))
   res.status(204)
 
   // Parse the request
